9c6dab176e531c7c2ad8a4129456b1ee8b3b783c
Jeevika Jarmarwala <jeevika04@gmail.com>
2020-04-17 17:51:03 -0500

    added database functionality

---
+++ public/application.js
@@ -0,0 +1,66 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDSYAo0FHhF79Aznk97990_OZCb3PRWxzM",
+    authDomain: "dontwingit.firebaseapp.com",
+    databaseURL: "https://dontwingit.firebaseio.com",
+    projectId: "dontwingit",
+    storageBucket: "dontwingit.appspot.com",
+    messagingSenderId: "411249906868",
+    appId: "1:411249906868:web:db9a4a1b192cb4814173e1",
+    measurementId: "G-W1T9SZ8256"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+// Reference to your entire Firebase database
+var myFirebase = firebase.database().ref();
+
+// firebase.auth().signInWithRedirec6t(provider);
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log("hello")
+
+  // Get a reference to the recommendations object of your Firebase.
+  // Note: this doesn't exist yet. But when we write to our Firebase using
+  // this reference, it will create this object for us!
+  var userRef = myFirebase.child("new_users");
+  // Push our first recommendation to the end of the list and assign it a
+  // unique ID automatically.
+  console.log("hello again")
+  userRef.push({
+      "email" : user.email,
+      "features" : [ "feature_id" ],
+      "name" : user.name,
+      "points" : 0,
+      "testing" : {
+          "automated" : {
+              "checked" : false,
+              "name" : "WebAIM"
+            }
+        },
+        "user_id" : "000000",
+        "wcag" : {
+            "checked" : [ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false ],
+            "conf_level" : [ "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable", "Acceptable", "Not applicable" ]
+        }
+    }
+  );
+  console.log("helllllo")
+
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
